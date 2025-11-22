ac80390ed5a451c08a8ab271c26c0441f4abf945
yLIue <2138347243@qq.com>
2025-11-22 16:05:28 +0800

    初始化项目,创建了项目说明

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Windborne_MP" 
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view>
+		<view class="status"></view>
+		<view class="In-title">
+			归<text class="In-title-off">风鸣</text>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.status{
+		height: var(--status-ber-height);
+	}
+	.In-title {
+		background-color: #f7f7f7;
+		height: 130rpx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 48rpx;
+	}
+	.In-title-off{
+		color:#949494 ;
+	}
+</style>
