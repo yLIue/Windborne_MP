b86ee7474cb28ad7cf0eba1b66dcd0c4edddfe21
yLIue <2138347243@qq.com>
2025-11-24 17:28:52 +0800

    markdown文件解析初步完成

--- js/Markdown.js
+++ js/Markdown.js
@@ -2,47 +2,71 @@ export default{
 	read
 }
 
-function read(_path){
+function read(_path,_callback){
 	let mdData
 	uni.request({
 		url:_path,
 		success: (res) => {
-			Resolve(res.data)
+			let temp = Resolve(res.data)
+			// console.log(temp)
+			_callback(temp)
 		}
 	})
 }
 
 function Resolve(_data){
-	let lines = _data.split('\n')
-	
-	let dict = {}
-	let values = []
-	let dictList = []
-	
-	for (let index in lines){
-		//去掉/r
-		lines[index] = lines[index].split('\r')[0]
+	let infos = _data.split('\n')
+	let lines = []
+	let layer = 0
+	for(let index in infos){
+		//去掉\r
+		let info = infos[index].split('\r')[0]
+		let dict = {
+			'type':'line',
+			'layer':layer,
+			'values':[]
+		}
+		
 		//忽略#和空行
-		let line = lines[index].split(' ')
-		if (line[0] == '#' || line == '')
+		if(info.split(' ')[0] == '#' || info == '')
 			continue
-		if (line[0] == '##'){
-			if(dict.hasOwnProperty('title')){
-				dict['value'] = dictList
-				values.push(dict)
-				dictList = []
-				dict = {}
-			}
-			dict['title'] = line[1]
+			
+		//title
+		if(info[0] == '#'){
+			let list
+			[list,layer] = getTitle(info)
+			dict['values'] = list
+			dict['layer'] = layer - 1
+		}
+			
+		//表格
+		else if(info[0] == '|')
 			continue
-		}
-		let dictValue = {
-			"type":"text",
-			"value":line
-		}
-		dictList.push(dictValue)
+		
+		//文本
+		else
+			dict.values.push(getText(info))
+			
+		lines.push(dict)
 	}
-	dict['value'] = dictList
-	values.push(dict)
-	// console.log(values)
+	return lines
+	// console.log(lines)
+}
+
+function getTitle(_info){
+	let infos = _info.split(' ')
+	let layer = infos[0].split('#').length - 2
+	let dict = {
+		'type':'title'
+	}
+	dict['values'] = infos[1]
+	return [[dict],layer]
+}
+
+function getText(_text){
+	let dict = {
+		'type':'text'
+	}
+	dict['values'] = _text
+	return dict
 }
--- pages/index.vue
+++ pages/index.vue
@@ -15,8 +15,6 @@
 <script>
 	import Data from '../config/config.json'
 	import Card from '../modules/Card'
-	import Markdown from '../js/Markdown'
-	// import Note from '../note/CiscoNote.md'
 	export default {
 		data() {
 			return {
@@ -25,7 +23,6 @@
 		},
 		onLoad() {
 			this.cards = Data.index
-			Markdown.read('note/CiscoNote.md')
 		},
 		methods: {
 		},
