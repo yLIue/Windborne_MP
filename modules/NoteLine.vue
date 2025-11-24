47751205415ab349ed03923d9aa8351a90f594a4
yLIue <2138347243@qq.com>
2025-11-24 17:29:38 +0800

    开始开发任务栏

---
+++ modules/NoteLine.vue
@@ -0,0 +1,38 @@
+<template>
+	<view>
+		<view v-for="(line,lineKey) in lines" :key="'line' + lineKey">
+			<view v-for="(item,itemKey) in line.values" :key="'item' + itemKey" class="item">
+				{{item.values}}
+			</view>
+			<!-- {{line.values}} -->
+		</view>
+	</view>
+</template>
+
+<script>
+	import Markdown from '../js/Markdown';
+	export default {
+		data() {
+			return {
+				lines:[]
+			}
+		},
+		onLoad() {
+			Markdown.read('note/CiscoNote.md',(note) => {
+				this.lines = note
+			})
+			
+			// this.lines = note
+		},
+		methods: {
+		},
+		components:{
+		}
+	}
+</script>
+
+<style>
+	.item{
+		margin-bottom: 50rpx;
+	}
+</style>
