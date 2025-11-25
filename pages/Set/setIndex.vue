9f0285a2f4773fe06f5efc27a5ba85ae34fa6dbe
yLIue <2138347243@qq.com>
2025-11-25 20:43:43 +0800

    开始写设置的主页面

--- pages/Set/setIndex.vue
+++ pages/Set/setIndex.vue
@@ -1,6 +1,13 @@
 <template>
 	<view>
 		<Title title="设置"></Title>
+		<view class="Set-list-box" v-for="(v,setlistkey) in list" :key="setlistkey" @click="To(v.to)"
+		hover-class="on" hover-stay-time="50">
+			<view>
+				<view class="Set-list-name">{{v.name}}</view>
+				<view class="Set-list-text">{{v.text}}</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -9,7 +16,18 @@
 	export default {
 		data() {
 			return {
-				
+				list:[
+					{
+						name:'关于小程序',
+						text:"版本号、更新日志",
+						to:'About'
+					},
+					{
+						name:'鸣谢名单',
+						text:"",
+						to:'Love'
+					}
+				]
 			}
 		},
 		methods: {
@@ -21,6 +39,23 @@
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	.Set-list-box{
+		height: 110rpx;
+		padding-left: 50rpx;
+		margin-bottom: 10rpx;
+		display: flex;
+		align-items: center;
+	}
+	.Set-list-name{
+		font-size: 35rpx;
+		color: #343434;
+	}
+	.Set-list-text{
+		font-size: 25rpx;
+		color: #606060;
+	}
+	.on{
+		background-color: #ebebeb;
+	}
 </style>
